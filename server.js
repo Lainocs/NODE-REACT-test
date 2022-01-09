5aa0cd941d8c174bb7d1a547e110fda1540b3a55
Nicolas de Garrigues <ndegarrigues@gmail.com>
2022-01-09 22:36:25 +0100

    :bug: FIX port deployment

--- server.js
+++ server.js
@@ -20,6 +20,6 @@ app.get('/*', (_, res) => {
     res.sendFile(path.join(__dirname, './client/build/index.html'))
 })
 
-app.listen(7000, () => {
-    console.log(`Server is running on port : ${PORT}`)
+app.listen(process.env.PORT || 7000, () => {
+    console.log(`Server is running`)
 })
